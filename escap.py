ab7bc3631e6cf169eb7095d107a5aad6acc01644
lucrenauld <45998966+lucrenauld@users.noreply.github.com>
2018-12-20 10:41:07 +0100

    Update escap.py

--- escap.py
+++ escap.py
@@ -59,7 +59,7 @@ print "\n"
 #port min et port max
 while True:
         #host = raw_input("IP or localhost : ")
-        host = "10.101.200.28"
+        host = "<host to scan>"
         x = raw_input("Min : ")
         y = raw_input("Max : ")
 
